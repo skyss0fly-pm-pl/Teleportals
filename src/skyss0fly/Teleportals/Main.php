0b3804f0e3f08c5f578653d539cecad350382dd0
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-11-18 15:24:35 +1000

    Create Main.php

---
+++ src/skyss0fly/Teleportals/Main.php
@@ -0,0 +1,13 @@
+<?php
+namespace skyss0fly\Teleportals;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+public function onEnable(): void {
+$this->getLogger()->info("Teleportals Intialized");
+  
+}
+  
+}
